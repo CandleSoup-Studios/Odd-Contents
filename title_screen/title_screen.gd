94e9c42244bd499e6588d6517033cd4b23ff326d
Lisa Koss <lisakoss@uw.edu>
2026-05-23 17:33:39 -0700

    :sparkles: feat: add title screen transitions

--- title_screen/title_screen.gd
+++ title_screen/title_screen.gd
@@ -1,5 +1,7 @@
 extends MarginContainer
 
+@onready var SceneTransitionAnimation = $TitleScreenSceneTransitionAnimation/AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,16 +11,21 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_new_game_pressed() -> void:
+	SceneTransitionAnimation.play("fade_in")
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://levels/overworld.tscn")
 
 func _on_load_game_pressed() -> void:
+	#get_tree().change_scene_to_file("")
 	pass # Replace with function body.
 
 func _on_settings_pressed() -> void:
+	#get_tree().change_scene_to_file("")
 	pass # Replace with function body.
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 func _on_credits_pressed() -> void:
+	#get_tree().change_scene_to_file("")
 	pass # Replace with function body.
