47b79f921f93f5ed162cfc0dc2b553198f060af0
Lisa Koss <lisakoss@uw.edu>
2026-06-03 22:53:25 -0700

    :lipstick: style: update HUD with  dynamic resizing

--- levels/overworld.gd
+++ levels/overworld.gd
@@ -9,7 +9,8 @@ func _on_signal(signal_passed_in):
 	match signal_passed_in:	
 		"hud_update":
 			$CanvasLayer.visible = true
-			$CanvasLayer/HUD/HUDBackground/HUDLabelTitle.text = "[i]" + Global.hud_display_title + "[/i]"
-			$CanvasLayer/HUD/HUDBackground/HUDLabelLocation.text = "[i]Head to " + Global.hud_display_location + "[/i]"
+			
+			$CanvasLayer/HUD/Container/HUDLabel.text = "[i]" + Global.hud_display_title + "[/i]" + "[br][font_size=14][i]Head to " + Global.hud_display_location + "[/i][/font_size]"
+			#$CanvasLayer/HUD/HUDBackground/HUDLabelLocation.text = "[i]Head to " + Global.hud_display_location + "[/i]"
 		"hud_hide":
 			$CanvasLayer.visible = false
