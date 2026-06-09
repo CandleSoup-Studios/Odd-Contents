d4742c153f78425ed0ac0507384bf90da56861af
Lisa Koss <lisakoss@uw.edu>
2026-06-08 18:10:46 -0700

    :sparkles: feat: add idle animations for all NPCs

--- npcs/harvey_jakes.gd
+++ npcs/harvey_jakes.gd
@@ -7,7 +7,21 @@ var npc_data = {
 func get_npc_name() -> String:
 	return npc_data["name"]
 	
+func _ready():
+	$IdleAnimation.animation_finished.connect(_on_idle_animation_finished)
+	
+func _on_idle_animation_finished():
+	$IdleAnimation.pause()
+	
+	if $IdleAnimation.animation == "hello":
+		$IdleAnimation.play("idle")
+	
 func _input(event) -> void:
 	if player_in_range and Input.is_action_just_pressed("dialogue_interaction"):
-		$AnimatedSprite2D.play("hello")
+		$IdleAnimation.play("hello")
 		super(event)
+	
+func _on_interaction_area_body_exited(body: Node2D) -> void:
+	super(body)
+	$IdleAnimation.play("goodbye")
+	$IdleAnimation.sprite_frames.set_animation_loop("idle", false)
--- npcs/remington_holloway.gd
+++ npcs/remington_holloway.gd
@@ -7,7 +7,14 @@ var npc_data = {
 func get_npc_name() -> String:
 	return npc_data["name"]
 	
+func _ready():
+	$IdleAnimation.animation_finished.connect(_on_idle_animation_finished)
+	
+func _on_idle_animation_finished():
+	$IdleAnimation.pause()
+	$IdleAnimation.play("idle")
+	
 func _input(event) -> void:
 	if player_in_range and Input.is_action_just_pressed("dialogue_interaction"):
-		$AnimatedSprite2D.play("surprise")
+		$IdleAnimation.play("surprise")
 		super(event)
