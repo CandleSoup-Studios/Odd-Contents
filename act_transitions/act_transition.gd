cae6336f87072974b4057f0680d65d40551a1233
Lisa Koss <lisakoss@uw.edu>
2026-06-09 17:04:04 -0700

    :sparkles: feat: add scene transitions between acts

--- act_transitions/act_transition.gd
+++ act_transitions/act_transition.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+extends Control
 
 @onready var SceneTransitionAnimation = $TitleScreenSceneTransitionAnimation/AnimationPlayer
 
@@ -11,6 +11,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_continue_button_pressed() -> void:
+	#$ContinueButton.free()
 	SceneTransitionAnimation.play("fade_in")
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://levels/overworld.tscn")
